de9272f72279f4e319f9724f259d5eb56f632a52
HeinMinZaw95 <auntymarlaundry@gmail.com>
2025-06-01 15:14:52 +0630

    Update main.js
    lock

--- main.js
+++ main.js
@@ -1,3 +1,94 @@
+const answer = 153519;
+const k1 = document.getElementById("one");
+const k2 = document.getElementById("two");
+const k3 = document.getElementById("three");
+const k4 = document.getElementById("four");
+const k5 = document.getElementById("five");
+const k6 = document.getElementById("six");
+const k7 = document.getElementById("seven");
+const k8 = document.getElementById("eight");
+const k9 = document.getElementById("nine");
+const k0 = document.getElementById("zero");
+const enter = document.getElementById("enter");
+const option = document.getElementById("option");
+
+let pass = "";
+
+k1.onclick = function(){
+    document.getElementById("display").textContent = pass;
+    const ans = document.getElementById("display").textContent ;
+    pass = pass + "1";
+    document.getElementById("display").textContent = pass;
+}
+k2.onclick = function(){
+    document.getElementById("display").textContent = pass;
+    const ans = document.getElementById("display").textContent ;
+    pass = pass + "2";
+    document.getElementById("display").textContent = pass;
+}
+k3.onclick = function(){
+        document.getElementById("display").textContent = pass;
+    const ans = document.getElementById("display").textContent ;
+    pass = pass + "3";
+    document.getElementById("display").textContent = pass;
+}
+k4.onclick = function(){
+        document.getElementById("display").textContent = pass;
+    const ans = document.getElementById("display").textContent ;
+    pass = pass + "4";
+    document.getElementById("display").textContent = pass;
+}
+k5.onclick = function(){
+        document.getElementById("display").textContent = pass;
+   const ans = document.getElementById("display").textContent ;
+   pass = pass + "5";
+   document.getElementById("display").textContent = pass;
+}
+k6.onclick = function(){
+        document.getElementById("display").textContent = pass;
+    const ans = document.getElementById("display").textContent ;
+    pass = pass + "6";
+    document.getElementById("display").textContent = pass;
+}
+k7.onclick = function(){
+        document.getElementById("display").textContent = pass;
+   const ans = document.getElementById("display").textContent ;
+    pass = pass + "7";
+    document.getElementById("display").textContent = pass;
+}
+k8.onclick = function(){
+        document.getElementById("display").textContent = pass;
+    const ans = document.getElementById("display").textContent ;
+   pass = pass + "8";
+   document.getElementById("display").textContent = pass;
+}
+k9.onclick = function(){
+        document.getElementById("display").textContent = pass;
+   const ans = document.getElementById("display").textContent ;
+    pass = pass + "9";
+    document.getElementById("display").textContent = pass;
+}
+k0.onclick = function(){
+        document.getElementById("display").textContent = pass;
+    const ans = document.getElementById("display").textContent ;
+   pass = pass + "0";
+   document.getElementById("display").textContent = pass;
+}
+enter.onclick = function(){
+
+    pass = Number(pass);
+    if (pass === 153519){
+        document.getElementById("display").textContent = "Welcome";
+        pass = "";
+        document.getElementById("lock").classList.add("lockclose");
+    }
+    else{
+         document.getElementById("display").textContent = "Something Wrong";
+         pass = "";
+    }
+    
+}
+
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = document.querySelector("i");
